6bc6e592ce12db1310c1bddc5ec2b029ad69967e
View <mine.my.new.email@gmail.com>
2022-10-06 16:54:06 +0800

    what

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 print("Please Enter Your name: ")
 name = input()
 print("Hello " + name)
+
+import math
+def get_roots(a,b,c):
+    d=b**2-4*a*c
+    if a==0:
+        if b==0:
+            if c==0:
+                print('1')
+            else:
+                print('2')
